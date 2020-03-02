6fd2d07f56ab9a39d6b001505ecf2cd2306a8fcb
StevenKiT <544736192@qq.com>
2020-03-02 16:10:02 -0600

    Initial commit

---
+++ src/com/home/practice4.java
@@ -0,0 +1,11 @@
+package com.home;
+
+import java.util.Scanner;
+
+public class practice4 {
+    public static void methodParam(Scanner sc) {
+        Scanner sca = new Scanner(System.in);
+        int input = sca.nextInt();
+        System.out.println("this is your input: " + input);
+    }
+}
