0c214f73c118faf3a6626f21c1a9c4527214079e
StevenKiT <544736192@qq.com>
2020-03-03 15:09:07 -0600

    New name(nothing change on code)

--- src/JavaLearning/Practice/CommonPractice/Main.java
+++ src/JavaLearning/Practice/CommonPractice/Main.java
@@ -58,7 +58,7 @@ public class Main {
 
         //practice3.Switch语句结合用户输入的结果选择练习
         int count = 10;
-        for (int i = 0; i <= count; i += 1) {
+        for (int i = 0; i <= count; i++) {
             switch (count) {
                 case 1:
                     System.out.println("Monday");
@@ -72,70 +72,75 @@ public class Main {
                     break;
             }
             // 如果还有下一个用户输入， 则打出用户输入时的关键字
-                String next = userInput.next();
-                System.out.println(next);
+            String next = userInput.next();
+            System.out.println(next);
+            break;
+        }
+
+        //practice4.使用匿名对象进行传参
+        UserInput.methodParam(new Scanner(System.in));
+
+        //practice5.对使用method的练习
+        //这里创建一个测试———类————Calculation的对象
+        Calculation cal = new Calculation();
+        //尝试打印1+....+100的值
+        System.out.println(cal.isOne());
+        //调用方法，打印10次——————“次数”
+        cal.keT();
+
+        //practice6.Random()的实例, 猜数字小游戏, 遍历集合从大到小.
+        Random r = new Random();
+        Scanner scan = new Scanner(System.in);
+        int countNum = 10;
+
+        //一共是10次猜测。
+        for (int h = 0; h < 10; h++) {
+            countNum--;
+            int RandomNum = r.nextInt(10) + 1;
+            System.out.print("请输入您的数字(1-10)： ");
+            int num = scan.nextInt();
+
+            //判断用户的猜测结果
+            if (num > RandomNum) {
+                System.out.println("输入的数字太大了");
+            } else if (RandomNum > num) {
+                System.out.println("输入的数字太小了");
+            } else {
+                System.out.println("恭喜你，中奖了");
                 break;
             }
-
-            //practice4.使用匿名对象进行传参
-            UserInput.methodParam(new Scanner(System.in));
-
-            //practice5.对使用method的练习
-            //这里创建一个测试———类————Calculation的对象
-            Calculation cal = new Calculation();
-            //尝试打印1+....+100的值
-            System.out.println(cal.isOne());
-            //调用方法，打印10次——————“次数”
-            cal.keT();
-
-            //practice6.Random()的实例, 猜数字小游戏, 遍历集合从大到小.
-            Random r = new Random();
-            Scanner scan = new Scanner(System.in);
-            int countNum = 10;
-
-            //一共是10次猜测。
-            for (int h = 0; h < 10; h++) {
-                countNum--;
-                int RandomNum = r.nextInt(10) + 1;
-                System.out.print("请输入您的数字(1-10)： ");
-                int num = scan.nextInt();
-
-                //判断用户的猜测结果
-                if (num > RandomNum) {
-                    System.out.println("输入的数字太大了");
-                } else if (RandomNum > num) {
-                    System.out.println("输入的数字太小了");
-                } else {
-                    System.out.println("恭喜你，中奖了");
-                    break;
-                }
-                //打印结果
-                System.out.println("随机数字是： " + RandomNum);
-                System.out.println("您还剩余： " + countNum + '\n');
-            }
-            /*=============================================================*/
-
-            //筛选一段随机数字的集合里的偶数
-            Random ran = new Random();
-            ArrayList<Integer> biggerList = new ArrayList<>();
-            ArrayList<Integer> smallList = new ArrayList<>();
-
-            int num;
-
-            //遍历集合， 添加随机数字到大集合里
-            for (int k = 0; k < 20; k++) {
-                num = ran.nextInt(30) + 1;
-                biggerList.add(num);
-
-                //条件判断，筛选大集合里的偶数值放进小集合
-                if (num % 2 == 0) {
-                    smallList.add(num);
-                }
-            }
-            //打印集合
-            System.out.println(smallList.size());
-            System.out.println(smallList);
+            //打印结果
+            System.out.println("随机数字是： " + RandomNum);
+            System.out.println("您还剩余： " + countNum + '\n');
         }
+        /*=============================================================*/
+
+        //筛选一段随机数字的集合里的偶数
+        Random ran = new Random();
+        ArrayList<Integer> biggerList = new ArrayList<>();
+        ArrayList<Integer> smallList = new ArrayList<>();
+
+        int num;
+
+        //遍历集合， 添加随机数字到大集合里
+        for (int k = 0; k < 20; k++) {
+            num = ran.nextInt(30) + 1;
+            biggerList.add(num);
+
+            //条件判断，筛选大集合里的偶数值放进小集合
+            if (num % 2 == 0) {
+                smallList.add(num);
+            }
+        }
+        //打印集合
+        System.out.println(smallList.size());
+        System.out.println(smallList);
+
+
+
+
+
     }
+}
 
 
