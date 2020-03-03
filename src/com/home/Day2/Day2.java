47b10df5508922cfeefc992bfcd886a784bee331
StevenKiT <544736192@qq.com>
2020-03-02 18:22:13 -0600

    Day2

--- src/com/home/Day2/Day2.java
+++ src/com/home/Day2/Day2.java
@@ -2,4 +2,19 @@ package com.home.Day2;
 
 public class Day2 {
 
+    /**
+     * @param args is not used
+     */
+    public static void main(String[] args) {
+
+        String str1 = "abc";
+        String str2 = "abc";
+
+        char[] charArray = {'a', 'b', 'c'};
+        String str3 = new String(charArray);
+
+        System.out.println(str1.equals(str2));//true
+        System.out.println(str1.equals(str3));//false
+        System.out.println(str3.equals(str2));//false
+    }
 }
