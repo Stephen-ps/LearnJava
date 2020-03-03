cf9f81e2ff59dde5b6b41290ede51a40a0822995
StevenKiT <544736192@qq.com>
2020-03-03 15:50:58 -0600

    New name(nothing change on code)

--- src/JavaLearning/Practice/CommonPractice/Student.java
+++ src/JavaLearning/Practice/CommonPractice/Student.java
@@ -1,37 +1,49 @@
 package JavaLearning.Practice.CommonPractice;
 
 public class Student {
+
     private String name;
     private int age;
     private boolean male;
 
     public Student(String name, int age, boolean male) {
+
         this.name = name;
         this.age = age;
         this.male = male;
     }
 
+    public Student() {
+
+    }
+
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public int getAge() {
+
         return age;
     }
 
     public void setAge(int age) {
+
         this.age = age;
     }
 
     public boolean isMale() {
+
         return male;
     }
 
     public void setMale(boolean male) {
+
         this.male = male;
     }
 }
